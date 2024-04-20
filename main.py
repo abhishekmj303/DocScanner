7b8676dd041c6e2ca1f7f34b5efd7ad4a372f2db
Abhishek M J <abhishekmj303@gmail.com>
2024-04-21 04:51:33 +0530

    Handle uploads

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+import shutil
+import uuid
 from typing import Annotated
 
-from fastapi import FastAPI, Request, UploadFile
+from fastapi import Cookie, FastAPI, Request, UploadFile
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -11,12 +14,32 @@ templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+async def read_root(
+    request: Request,
+    user_id: Annotated[str | None, Cookie()] = None,
+):
+    response = templates.TemplateResponse("index.html", {"request": request})
+    print(f"User ID: {user_id}")
+    if user_id is None:
+        user_id = str(uuid.uuid4())
+        response.set_cookie(key="user_id", value=user_id, httponly=True)
+        print(f"Set cookie: {user_id}")
+    return response
 
 
-@app.post("/upload/", response_class=HTMLResponse)
-async def create_upload_file(request: Request, file: UploadFile):
-    with open(f"static/{file.filename}", "wb") as buffer:
-        buffer.write(await file.read())
-    return templates.TemplateResponse("upload.html", {"request": request})
+@app.post("/upload/")
+async def create_upload_file(
+    request: Request, 
+    user_id: Annotated[str, Cookie()],
+    files: list[UploadFile], 
+):
+    print(f"User ID: {user_id}")
+    print({"filenames": [file.filename for file in files]})
+    if os.path.exists(f"uploads/{user_id}"):
+        shutil.rmtree(f"uploads/{user_id}")
+    os.makedirs(f"uploads/{user_id}", exist_ok=True)
+    for i, file in enumerate(files):
+        file_path = f"uploads/{user_id}/{i}_{file.filename}"
+        with open(file_path, "wb") as buffer:
+            buffer.write(await file.read())
+    return ""
